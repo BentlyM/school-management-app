91583c7552e44204e6e7abf42e71a99fc5c81b14
BentlyM <metayerbently@gmail.com>
2024-11-27 09:09:20 -0500

    completed ui for admin page including calendars and announcement feeds

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,7 @@
+import Announcements from '@/components/Announcements';
 import AttendanceChart from '@/components/AttendanceChart';
 import CountChart from '@/components/CountChart';
+import EventCalendar from '@/components/EventCalendar';
 import FinanceChart from '@/components/FiannaceChart';
 import UserCard from '@/components/UserCard';
 import React from 'react';
@@ -34,8 +36,8 @@ const AdminPage = () => {
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        {/* <EventCalendar />
-        <Announcements /> */}
+        <EventCalendar />
+        <Announcements />
       </div>
     </div>
   );
